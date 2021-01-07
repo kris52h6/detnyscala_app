886c7cf05b03f7d1f9e515fe407dea8e25c06121
Victor Øbro <vict889f@stud.kea.dk>
2021-01-07 13:02:04 +0100

    start style

---
+++ script2.js
@@ -0,0 +1,21 @@
+
+"use strict";
+
+window.addEventListener("load", init2);
+
+function init2(){
+
+
+}
+
+
+// Open and close barlist
+function openNav() {
+    document.getElementById("drinks").style.width = "100vw";
+    // document.getElementById("drinks").style.padding = "60px 20px 0 20px";
+  }
+  
+  function closeNav() {
+    document.getElementById("drinks").style.width = "0";
+    // document.getElementById("drinks").style.padding = "60px 0 0 0";
+  }
