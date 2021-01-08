5adcc987d3f8ad784ec06fd6236a0f8b3e7c050c
Kristian <kris52h6@stud.kea.dk>
2021-01-08 15:13:03 +0100

    add to cart

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ async function getJson() {
 function displayDrinks() {
   let counter = 0;
   modtagerKloner.innerHTML = "";
-  console.log(filterDrinks);
   myJson.feed.entry.forEach((drink) => {
     if (filterDrinks == "alle" || filterDrinks == drink.gsx$kategori.$t) {
       const klon = skabelon.cloneNode(true).content;
@@ -39,33 +38,33 @@ function displayDrinks() {
       klon.querySelector(".add").id = counter;
       klon.querySelector(".plus").id = counter;
       klon.querySelector(".minus").id = counter;
+      klon.querySelector(".quantity").id = counter;
 
       klon.querySelector(".minus").addEventListener("click", () => {
         drinkMinus();
       });
 
-      klon.querySelector(".plus").addEventListener("click", () => {
-        drinkPlus();
+      klon.querySelector(".plus").addEventListener("click", (event) => {
+        drinkPlus(event);
       });
       klon.querySelector(".add").addEventListener("click", (event) => {
         countDrinks(drink, event);
       }); // klon.querySelector("img").src = `${drink.gsx$billedelink}`;
 
       modtagerKloner.appendChild(klon);
+      counter++;
     }
-    counter++;
   });
 }
 
 function countDrinks(drink, event) {
   let drinkAmount = document.querySelectorAll(".quantity")[event.target.id].value;
-
+  console.log(document.querySelectorAll(".quantity")[event.target.id].value);
   totalAmount.push(drinkAmount);
   let drinkObject = {
     name: drink.gsx$navn.$t,
     amount: drinkAmount,
   };
-  console.log(drinkObject);
   cartArray.push(drinkObject);
   localStorage.setItem("order", JSON.stringify(cartArray));
 }
@@ -76,22 +75,19 @@ function drinkMinus() {
   }
 }
 
-function drinkPlus() {
+function drinkPlus(event) {
   if (document.querySelectorAll(".quantity")[event.target.id].value < 9) {
     document.querySelectorAll(".quantity")[event.target.id].value++;
   }
 }
 
 function handleFilter() {
-  /* tilføjer klassen valgt til hvert element eller fjerner valgt */
   document.querySelectorAll(".valgt").forEach((element) => {
     element.classList.remove("valgt");
   });
 
-  /* tilføjer variablen filteralbum til datasetet kategori og tilføjer valgt */
   filterDrinks = this.dataset.kategori;
   this.classList.add("valgt");
   document.querySelector("h1").textContent = filterDrinks;
-  /* kalder funktionen visGS */
   displayDrinks();
 }
