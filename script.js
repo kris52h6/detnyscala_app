c141fa7579150cb767996719c90f45b3ae45107b
Victor Øbro <vict889f@stud.kea.dk>
2021-01-09 12:23:10 +0100

    eh

--- script.js
+++ script.js
@@ -91,3 +91,14 @@ function handleFilter() {
   document.querySelector("h1").textContent = filterDrinks;
   displayDrinks();
 }
+
+// Open and close barlist
+function openNav() {
+  document.getElementById("drinks").style.width = "100vw";
+  // document.getElementById("drinks").style.padding = "60px 20px 0 20px";
+}
+
+function closeNav() {
+  document.getElementById("drinks").style.width = "0";
+  // document.getElementById("drinks").style.padding = "60px 0 0 0";
+}
